99cb8bde877af67bbdc0afc129f286b6cbf8b4b1
aleksandar popovic <popovicsail@gmail.com>
2025-06-24 19:28:07 +0200

    hotfix br 16134

--- app/tours/pages/toursEdit/toursEdit.ts
+++ app/tours/pages/toursEdit/toursEdit.ts
@@ -1,10 +1,14 @@
 import { Tour } from "../../models/tour.model.js";
 import { ToursServices } from "../../services/tours.services.js"
+import { ToursUtils } from "../../utils/tours.utils.js"
 const toursServices = new ToursServices()
+const toursUtils = new ToursUtils()
+
 const url = window.location.search
 const searchParams = new URLSearchParams(url)
 const guideId = parseInt(searchParams.get("guideId"))
 const tourId = parseInt(searchParams.get("tourId"))
+
 const tourName = document.querySelector("#tourName") as HTMLInputElement
 const tourDescription = document.querySelector("#tourDescription") as HTMLInputElement
 const tourDateTime = document.querySelector("#tourDateTime") as HTMLInputElement
@@ -12,12 +16,51 @@ const tourMaxGuests = document.querySelector("#tourMaxGuests") as HTMLInputEleme
 
 
 function tourFormInitialize(guideId: number, tourId: number): void {
+    tourName.addEventListener("blur", () => {
+        toursUtils.validationSingleInput(tourName)
+        validationTourFormData()
+    })
+
+    tourDescription.addEventListener("blur", () => {
+        toursUtils.validationSingleInput(tourDescription)
+        validationTourFormData()
+    })
+
+    tourDateTime.addEventListener("blur", () => {
+        toursUtils.validationSingleInput(tourDateTime)
+        validationTourFormData()
+    })
+
+    tourMaxGuests.addEventListener("blur", () => {
+        toursUtils.validationSingleInput(tourMaxGuests)
+        validationTourFormData()
+    })
+
     const submitButton = document.getElementById("submitButton") as HTMLButtonElement
     submitButton.addEventListener("click", () => submitTourFormData(guideId, tourId));
     fillTourFormData(tourId)
 
     const cancelButton = document.getElementById("cancelButton") as HTMLButtonElement
     cancelButton.addEventListener("click", () => window.location.href = `../toursOverview/toursOverview.html?guideId=${guideId}`)
+}
+
+
+function validationTourFormData() {
+    const submitButton = document.getElementById("submitButton") as HTMLButtonElement
+
+    const tourNameFlag = toursUtils.validationFinal(tourName)
+    const tourDescriptionFlag = toursUtils.validationFinal(tourDescription)
+    const tourDateTimeFlag = toursUtils.validationFinal(tourDateTime)
+    const tourMaxGuestsFlag = toursUtils.validationFinal(tourMaxGuests)
+
+    if (!tourNameFlag || !tourDescriptionFlag || !tourDateTimeFlag || !tourMaxGuestsFlag) {
+        submitButton.disabled = true;
+        submitButton.style.backgroundColor = "grey"
+        return;
+    }
+    submitButton.disabled = false;
+    submitButton.style.backgroundColor = "green"
+    return;
 }
 
 
@@ -44,38 +87,10 @@ function submitTourFormData(guideId: number, tourId: number): void {
 
     const newTour: Tour = { name, description, dateTime, maxGuests, status, guideId }
 
-    validation(newTour)
-
     toursServices.update(tourId, newTour)
         .then(() => window.location.href = `../toursOverview/toursOverview.html?guideId=${guideId}`)
         .catch(error => console.error(error.status, error.message))
 }
-
-
-function validation(newTour: Tour) {
-    const tourNameError = document.querySelector("#tourNameError")
-    tourNameError.textContent = ''
-    const tourDescriptionError = document.querySelector("#tourDescriptionError")
-    tourDescriptionError.textContent = ''
-    const tourDateTimeError = document.querySelector("#tourDateTimeError")
-    tourDateTimeError.textContent = ''
-    const tourMaxGuestsError = document.querySelector("#tourMaxGuestsError")
-    tourMaxGuestsError.textContent = ''
-
-    if (newTour.name.trim() === '') {
-        tourNameError.textContent = "Name field is required!"
-    }
-    if (newTour.description.trim() === '') {
-        tourDescriptionError.textContent = "Description field is required!"
-    }
-    if (newTour.dateTime.trim() === '') {
-        tourDateTimeError.textContent = "Date field is required!"
-    }
-    if (newTour.maxGuests === null || newTour.maxGuests === 0) {
-        tourMaxGuestsError.textContent = "Max guests field is required!"
-    }
-}
-
 
 document.addEventListener("DOMContentLoaded", () => {
     tourFormInitialize(guideId, tourId);
