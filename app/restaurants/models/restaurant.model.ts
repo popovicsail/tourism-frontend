ba9bf186cf38e37a69fd16411f58f1c847008dc5
Kilarus97 <zecmirko97@gmail.com>
2025-06-24 04:41:23 +0200

    Feat/Restaurants zavrsen

---
+++ app/restaurants/models/restaurant.model.ts
@@ -0,0 +1,11 @@
+export interface Restaurant {
+id?:number
+name:string
+description:string
+capacity:number
+imageUrl:string
+latitude:number
+longitude:number
+status:string
+ownerID:number
+}
---
+++ app/restaurants/restaurants.ts
@@ -0,0 +1,87 @@
+import { Restaurant } from "./models/restaurant.model.js";
+import { RestaurantService } from "./services/restaurants.service.js";
+
+const restoranService = new RestaurantService();
+const dodajBtn = document.querySelector("#Dodaj") as HTMLElement
+
+
+function ispisiRestorane(){
+    const tabela = document.querySelector('#usersBody') as HTMLTableElement
+    
+    if (!tabela) {
+        console.error('Tabela sa ID-jem "usersBody" nije pronađena.');
+        return;
+    }
+    
+    tabela.innerHTML = ''
+
+    restoranService.getByOwner().then((response) => {
+        const nizRestorana: Restaurant[] = response; {
+         nizRestorana.forEach((restoran) => {
+            const noviRed = tabela.insertRow()
+    
+            const idCell = noviRed.insertCell();
+            idCell.textContent = restoran.id.toString();
+    
+            const nazivCell = noviRed.insertCell();
+            nazivCell.textContent = restoran.name;
+    
+            const opisCell = noviRed.insertCell();
+            opisCell.textContent = restoran.description;
+    
+            const capacityCell = noviRed.insertCell();
+            capacityCell.textContent = restoran.capacity.toString();
+
+            const slikaCell = noviRed.insertCell();
+            const img = document.createElement("img");
+
+            img.src = restoran.imageUrl;
+            img.alt = restoran.name || "Slika restorana";
+            img.width = 100;
+
+            slikaCell.appendChild(img);
+
+            const latitudeCell = noviRed.insertCell();
+            latitudeCell.textContent = restoran.latitude.toString();
+
+            const longitudeCell = noviRed.insertCell();
+            longitudeCell.textContent = restoran.longitude.toString();
+
+            const statusCell = noviRed.insertCell();
+            statusCell.textContent = restoran.status;
+
+            const izmeniCell = noviRed.insertCell();
+            const izmeniBtn = document.createElement('button');
+            izmeniBtn.textContent = 'Izmeni';
+
+            izmeniBtn.addEventListener('click', () => {
+                const red = izmeniBtn.closest('tr')!;
+                const id = parseInt(red.cells[0].textContent!);
+                window.location.href = `pages/restaurantsUpdate/restaurantsUpdate.html?id=${id}`;
+            });
+
+            izmeniCell.appendChild(izmeniBtn);
+
+            const izbrisiCell = noviRed.insertCell();
+            const izbrisiBtn = document.createElement('button');
+            izbrisiBtn.textContent = 'Izbrisi';
+            izbrisiCell.appendChild(izbrisiBtn);
+
+            izbrisiBtn.addEventListener('click', () => {
+                const red = izbrisiBtn.closest('tr')!;
+                const id = parseInt(red.cells[0].textContent!);
+                restoranService.deleteUser(id);
+                window.location.href = '../index.html'
+            });
+            
+         });
+     }}).catch((error) => {
+         console.error('Greška prilikom učitavanja restorana:', error);
+     });
+}
+
+dodajBtn.addEventListener('click', () => {
+    window.location.href = "pages/restaurantsCreate/restaurantsCreate.html"; 
+  });
+
+document.addEventListener('DOMContentLoaded', ispisiRestorane);
